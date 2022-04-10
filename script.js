8ba091c0d7cf406069fbf47f7676a4da3ac7b9b7
Dietrich Varez <dietrich1899@gmail.com>
2022-04-09 20:57:49 -0700

    boxes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello world");
+
+var apiKey = "ad7e308f01fc88ecad0f71475a7096e2"
+
+
+
+
+
+
+// when a search for a city, presented with current and future condiitons for that city, and city is added to the search history.
+// when current weather conditions for a city are presented, city name, the date, an icon representation, the temperature, the humidity, the wind speed, the UV index.
+// when the UV index is presented, a color is indicative whether the condiions are favorable, moderate, or severe.
+// when future weather conditions for a city, a five day forcast dispays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity.
+// when clicked on a city in the search history, again presented with current and future conditions for that city.
